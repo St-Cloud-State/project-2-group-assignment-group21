8f72734fcf563bf81510a72265aa3638e65e1d16
Thomas <77698354+Slump5@users.noreply.github.com>
2025-11-07 14:57:43 -0600

    Change ManagerMenuState to implement UIState interface

--- Project2Stage1/src/com/stage1/ManagerMenuState.java
+++ Project2Stage1/src/com/stage1/ManagerMenuState.java
@@ -1,9 +1,10 @@
 package com.stage1;
 
-public class ManagerMenuState extends UIState
-{
+public class ManagerMenuState implements UIState {
     @Override
     public void run() {
-
+        System.out.println("\n--- MANAGER MENU ---");
+        System.out.println("(placeholder – implemented by teammate later)");
+        UIContext.instance().changeState(UIContext.LOGIN_STATE);
     }
 }
