1812596aa3f4ace15ffb37b4665547794693c27c
Thomas <77698354+Slump5@users.noreply.github.com>
2025-11-07 14:56:50 -0600

    Change ClientMenuState to implement UIState interface

--- Project2Stage1/src/com/stage1/ClientMenuState.java
+++ Project2Stage1/src/com/stage1/ClientMenuState.java
@@ -1,9 +1,11 @@
 package com.stage1;
 
-public class ClientMenuState extends UIState
-{
+public class ClientMenuState implements UIState {
     @Override
     public void run() {
-
+        System.out.println("\n--- CLIENT MENU ---");
+        System.out.println("(placeholder – implemented by teammate later)");
+        UIContext.instance().changeState(UIContext.LOGIN_STATE);
     }
 }
+
